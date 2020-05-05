d6d78b13c39f6dd293c8d62d8136e39a7e92a827
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-05-05 18:14:29 +0900

    Stylinig to do list

---
+++ TodoList/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import { render } from 'react-dom';
+
+const { height, width } = Dimensions.get("window");
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content"></StatusBar>
+      <Text style={styles.title}>To DO</Text>
+      <View>
+        <TextInput style={styles.card}></TextInput>
+      </View>
+    </View>
+  );
+}
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F23657',
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 40,
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor:"rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset:{
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    }
+    )
+  },
+});
