a0ab98f2e0216e64055006a00b68ced685f8b4a2
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-05-05 14:23:01 +0900

    Test react-native

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import {Alert} from "react-native";
 import { StyleSheet, Text, View } from 'react-native';
 import Loading from './Loading';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <Loading />
-  );
+export default class extends React.Component {
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync();
+      console.log("location");
+    }catch (error){
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    return <Loading />
+  }
 }
+
