d195011d06807213196bfe6491aa92729ae9a892
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-05-21 18:41:47 +0900

    Get weather information & hide api key

--- getWeather/App.js
+++ getWeather/App.js
@@ -2,10 +2,20 @@ import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
+import axios from "axios";
+import { WEATHER_API_KEY } from "react-native-dotenv";
+
+const API_KEY = WEATHER_API_KEY;
 
 export default class extends React.Component {
   state = {
     isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
   };
   getLocation = async () => {
     try {
@@ -13,6 +23,7 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
